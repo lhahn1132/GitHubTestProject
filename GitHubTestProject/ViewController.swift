7e4d36e8ef9c8c508761a91ccfb2bb5dd70759a6
Lucy Hahn <lhahn1132@stu.d214.org>
2020-10-22 13:22:38 -0700

    Initial Commit

---
+++ GitHubTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubTestProject
+//
+//  Created by user171627 on 10/22/20.
+//  Copyright © 2020 user1132. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
